5a0081d94ef996208d1de7d340f62b46603af6c7
Psychopath <sn05coast@gmail.com>
2018-08-28 01:23:03 +0300

    переделал структуру

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 console.log('Hello');
 
 import './style/style.css';
-import './js/auth'
+import render from './templates/friends.hbs'
+
+VK.init({
+    apiId: 6674759
+});
+function auth() {
+    return new Promise((resolve, reject) => {
+        VK.Auth.login(data => {
+            if (data.session) {
+                resolve();
+            } else {
+                reject(new Error('Не удалось авторизоваться'));
+            }
+        }, 2);
+    });
+}
+
+function callApi(method, params) {
+    params.v = '5.76';
+
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, (data) => {
+            if (data.error) {
+                reject(data.error);
+            } else {
+                resolve(data.response)
+            }
+        })
+    })
+}
+
+auth()
+    .then(() => {
+        return callApi('friends.get', { fields: 'photo_100 , first_name, last_name' });
+    })
+    .then(friends => {
+        const container = document.querySelector('.list');
+
+		container.innerHTML = render({ friends });
+    })
